88eee5e05e7f98e371245914f5daa6ed2b12385c
caizkun <caizkun@gmail.com>
2017-12-06 11:43:34 -0600

    fix bug in setup-vim

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # set up vim
-cp -r vim/ ~/.vim/
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+fi
+
+cp -r vim/* ~/.vim/
 cp -r vimrc ~/.vimrc
+
+vim +PluginInstall +qall
+
+printf "\nYou are all set!\n"
